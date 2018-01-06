c89531adac994115c0b61d64fb2c3c1a85b2d010
noney1412 <nonpanpila@gmail.com>
2018-01-06 21:30:22 +0700

    :art: print

--- MyC++/first_project/main.cpp
+++ MyC++/first_project/main.cpp
@@ -1,3 +1,10 @@
+#include <iostream>
+using namespace std;
+
 int main() {
-    
+  cout << " Chanon Panpila " << endl;
+  cout << " My age 20 \n";
+  cout << " Address Changarkatutid 6 \n";
+
+  return 0;
 }
