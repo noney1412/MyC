88cefd8a7d64ef1efd042fe1729c1643453b6e19
noney1412 <nonpanpila@gmail.com>
2018-01-07 00:32:18 +0700

    test

--- MyC++/first_project/main.cpp
+++ MyC++/first_project/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 
 using namespace std;
-
 // cout = console output_stream object.
-int main() {
-  // 1 bytes = 8 bits
-  int a = 2500;
-  cout << sizeof(a);
-  return 0;
+int main()
+{
+    // 1 bytes = 8 bits
+    short a;
+    int b;
+    cout << a << b;
+    return 0;
 }
