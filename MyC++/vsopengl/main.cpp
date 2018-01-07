e8640d2f0fe4eb5e258d927ad4f7f6456ac079ad
noney1412 <nonpanpila@gmail.com>
2018-01-07 22:14:14 +0700

    c++

--- MyC++/vsopengl/main.cpp
+++ MyC++/vsopengl/main.cpp
@@ -1,14 +1,7 @@
 #include <iostream>
 
 using namespace std;
-// cout = console output_stream object.
-int main() {
-  // 1 bytes = 8 bits
-  short a;
-  int b{4};
-  cout << a << b <<endl;
-  cout << "My Name is Chanon Panpila 555 \n" ; 
-  cin >> a;
-  cout << "My Age is 25" << a ; 
-  return 0; 
-} 
+
+int main(){
+    cout << "Chanon Panpila" ; 
+}
